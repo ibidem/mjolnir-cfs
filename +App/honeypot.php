4a6f85e637339344d9bd822519824dad18715809
srcspider <source.spider@gmail.com>
2012-11-05 16:09:01 +0200

    trace is now properly formatted on windows

--- +App/honeypot.php
+++ +App/honeypot.php
@@ -1,6 +1,11 @@
-<?php # IDE honeypot :)
+<?php namespace app;
 
-namespace app;
+// This is an IDE honeypot. It tells IDEs the class hirarchy, but otherwise has
+// no effect on your application. :)
+
+// HowTo: order honeypot -n 'mjolnir\cfs'
 
 class CFS extends \mjolnir\cfs\CFS {}
+class CFSCompatible extends \mjolnir\cfs\CFSCompatible {}
 class Kohana3_Bridge extends \mjolnir\cfs\Kohana3_Bridge {}
+class Mjolnir extends \mjolnir\cfs\Mjolnir {}
