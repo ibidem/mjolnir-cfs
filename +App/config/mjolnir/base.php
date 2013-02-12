ce4ee43dc08033302b0aa6a95b646b663a6a2760
srcspider <source.spider@gmail.com>
2013-02-12 17:52:18 +0200

    moved mjolnir/base from base module to cfs module

--- +App/config/mjolnir/base.php
+++ +App/config/mjolnir/base.php
@@ -1,5 +1,15 @@
 <?php return array
 	(
+		'domain' => null,
+		'path' => '/',
+
+		'timezone' => 'Europe/London',
+		'charset' => 'UTF8',
+		'locale.lang' => 'en_US',
+		'default.protocol' => 'http://', # eg. //, http://, https://
+
+		'development' => false,
+	
 		// Read & Write for owner, Read for everybody else
 		'default.file.permissions' => 0644, 
 	
