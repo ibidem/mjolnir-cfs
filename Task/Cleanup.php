af9f2f1d9c722183ac12e0be3a171ffd1ac788c3
srcspider <source.spider@gmail.com>
2013-02-28 10:53:00 +0200

    cleanup no longer removes dot files (eg. .gitignore, etc)

--- Task/Cleanup.php
+++ Task/Cleanup.php
@@ -19,13 +19,48 @@ class Task_Cleanup extends \app\Instantiatable implements \mjolnir\types\Task
 		\app\Task::consolewriter($this->writer);
 		
 		$this->writer->writef(' Reseting cache.')->eol();
-		\app\Stash::flush();
-		$this->writer->writef(' Removing logs.')->eol();
-		\app\Filesystem::purge(APPPATH.'logs');
+		\app\Stash_File::instance()->flush();
+		\app\Stash_Memcache::instance()->flush();
+		\app\Stash_Memcached::instance()->flush();
+		
+		# Remove Log files
+		
+		$this->writer->writef(' Removing logs.')->eol();	
+		$log_files = \app\Filesystem::matchingfiles(APPPATH.'logs', '#^[^\.].*$#');
+		
+		foreach ($log_files as $file)
+		{
+			$this->writer->writef('  - removing '. \str_replace('\\', '/', \str_replace(DOCROOT, '', $file)))->eol();
+			\unlink($file);
+		}
+		
+		\app\Filesystem::prunedirs(APPPATH.'logs');
+		
+		# Remove Cache files
+		
 		$this->writer->writef(' Removing cache files.')->eol();
-		\app\Filesystem::purge(APPPATH.'cache');
+		$cache_files = \app\Filesystem::matchingfiles(APPPATH.'cache', '#^[^\.].*$#');
+		
+		foreach ($cache_files as $file)
+		{
+			$this->writer->writef('  - removing '. \str_replace('\\', '/', \str_replace(DOCROOT, '', $file)))->eol();
+			\unlink($file);
+		}
+		
+		\app\Filesystem::prunedirs(APPPATH.'cache');
+		
+		# Remove Temporary file
+		
 		$this->writer->writef(' Removing temporary files.')->eol();
-		\app\Filesystem::purge(APPPATH.'tmp');
+		$tmp_files = \app\Filesystem::matchingfiles(APPPATH.'tmp', '#^[^\.].*$#');
+		
+		foreach ($tmp_files as $file)
+		{
+			$this->writer->writef('  - removing '. \str_replace('\\', '/', \str_replace(DOCROOT, '', $file)))->eol();
+			\unlink($file);
+		}
+		
+		\app\Filesystem::prunedirs(APPPATH.'tmp');
 	}
 
 } # class
